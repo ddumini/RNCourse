b07a98e4696dca6db62955ad45201dc7005d70a2
ddumini <cukelover52@gmail.com>
2026-04-23 09:33:56 +0900

    intro

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start ddumini!</Text>
-      <StatusBar style='auto' />
+      <View>
+        <Text>Open up App.js to start ddumini!</Text>
+      </View>
+      <Text>OnePiece</Text>
+      <Button title='Do anything yet' onPress={() => alert('Button pressed')} />
     </View>
   )
 }
